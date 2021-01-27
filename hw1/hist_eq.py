f38272b535c0bedbb4f4664085cf33e8e2ec96b5
Minsung <ms98.cho@gmail.com>
2021-01-26 23:36:27 -0800

    I am giving up on the second problem. The part c is not started.

--- hw1/hist_eq.py
+++ hw1/hist_eq.py
@@ -3,26 +3,21 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-"""
-Script that applies histogram equalization to a greyscale image.
-Specify an input image and name for the output image below.
-When script is ran two histograms will be displayed:
- - The colour distribution of the original image
- - The colour distribution of the new image
-Accompanying blog post: https://ukdevguy.com/...
-"""
+# Setting the input and output files.
+IN_FILE = 'hw1\sample_images\jetplane.png'
+OUT_FILE = 'hw1\sample_images\jetplane_output64.png'
 
-# Only two constants that need setting before running
-IN_FILE = "sample_images/jetplane.png"
-OUT_FILE = "sample_images/jetplane_output.png"
+# Set the bin size
+bin = 64
 
+# Make histogram from image
 def make_histogram(img):
-    """ Take a flattened greyscale image and create a historgram from it """
     histogram = np.zeros(256, dtype=int)
     for i in range(img.size):
         histogram[img[i]] += 1
     return histogram
 
+# make cumulative sum
 def make_cumsum(histogram):
     """ Create an array that represents the cumulative sum of the histogram """
     cumsum = np.zeros(256, dtype=int)
@@ -31,17 +26,15 @@ def make_cumsum(histogram):
         cumsum[i] = cumsum[i-1] + histogram[i]
     return cumsum
 
+# make mapping
 def make_mapping(cumsum):
-    """ Create a mapping s.t. each old colour value is mapped to a new
-        one between 0 and 255. Mapping is created using:
-         - M(i) = max(0, round((grey_levels*cumsum(i))/(h*w))-1)
-        where g_levels is the number of grey levels in the image """
     mapping = np.zeros(256, dtype=int)
-    grey_levels = 256
+    grey_levels = bin
     for i in range(grey_levels):
         mapping[i] = max(0, round((grey_levels*cumsum[i])/(IMG_H*IMG_W))-1)
     return mapping
 
+# apply mapping
 def apply_mapping(img, mapping):
     """ Apply the mapping to our image """
     new_image = np.zeros(img.size, dtype=int)
@@ -53,19 +46,19 @@ def apply_mapping(img, mapping):
 pillow_img = Image.open(IN_FILE)
 IMG_W, IMG_H = pillow_img.size
 
-# Read in and flatten our greyscale image, calculate the histogram,
-# cumulative sum, mapping and then apply the mapping to create a new image
+img = np.array(pillow_img).flatten()
+histogram = make_histogram(img)
+cumsum = make_cumsum(histogram)
+mapping = make_mapping(cumsum)
+new_image = apply_mapping(img, mapping)
+
+# Read in and flatten our greyscale image
 print('Original image:',np.array(pillow_img))
 img = np.array(pillow_img).flatten()
 print('Flattened image',np.array(img))
 print('Original image width:', IMG_W)
 print('Original image height:',IMG_H)
 print('Flatten image size: 512 * 512 = ', img.size)
-
-histogram = make_histogram(img)
-cumsum = make_cumsum(histogram)
-mapping = make_mapping(cumsum)
-new_image = apply_mapping(img, mapping)
 
 # Save the image
 output_image = Image.fromarray(np.uint8(new_image.reshape((IMG_H, IMG_W))))
@@ -75,10 +68,10 @@ output_image.save(OUT_FILE)
 x_axis = np.arange(256)
 fig = plt.figure()
 fig.add_subplot(2, 2, 1)
-before = mpimg.imread('sample_images/jetplane.png')
+before = mpimg.imread('hw1\sample_images\jetplane.png')
 beforeplot = plt.imshow(before, cmap = 'gray')
 fig.add_subplot(2, 2, 2)
-after = mpimg.imread('sample_images/jetplane_output.png')
+after = mpimg.imread('hw1\sample_images\jetplane_output64.png')
 afterplot = plt.imshow(after, cmap = 'gray')
 fig.add_subplot(2, 2, 3)
 a = plt.bar(x_axis, histogram)
